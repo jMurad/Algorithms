ac82078e330bc984c7c4d59c8ed669c75e35dab4
JMurad <jml.murad@gmail.com>
2024-07-04 15:21:30 +0300

    del comment

--- internal/service/hash-table/hash-table.go
+++ internal/service/hash-table/hash-table.go
@@ -87,20 +87,6 @@ func hash256(key string, l int) int {
 	return sum % l
 }
 
-// func hash(key string, l int) int {
-// 	filter := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-// 	primeNums := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
-// 		59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137,
-// 		139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,
-// 		227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307}
-// 	sum := 0
-// 	for _, k := range key {
-// 		index := strings.IndexRune(filter, k)
-// 		sum += primeNums[index]
-// 	}
-// 	return sum % l
-// }
-
 func set(store []ll.List, key string, value interface{}) bool {
 	list := &store[hash256(key, len(store))]
 
